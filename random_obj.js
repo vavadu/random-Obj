206eff0e8ec77acc85207e45bb50534f6a82a4d2
VavaDu <vavadulia@gmail.com>
2021-02-16 14:56:41 +0200

    fixed review comments

---
+++ random_obj.js
@@ -0,0 +1,30 @@
+const FIRST_NAME = ['Dima', 'Lera', 'Sasha', 'Jama'];
+const LAST_NAME = ['Ivanov', 'Serov', 'Pupkin', 'Gele'];
+const SEX = ['male', 'female']; 
+
+
+
+function getRandomItem(array) {
+    return array[getRandomNumber(array.length)]; 
+}
+function getRandomNumber(max) {
+    return  Math.floor(Math.random()*max);
+}
+
+function generatePeopleArray(n) {   
+    let result = [];
+    for (let i = 0; i<n ; i++) {
+        let person = {
+            lastname: getRandomItem(LAST_NAME),
+            firstname: getRandomItem(FIRST_NAME),
+            age: getRandomNumber(100),
+            sex: getRandomItem(SEX),      
+        };
+
+        result.push(person); 
+    }
+    return result;
+   
+}
+console.log(generatePeopleArray(100));
+
