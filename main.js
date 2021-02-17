b34de83aef1638708408cbb97d520025882b2eb7
VavaDu <vavadulia@gmail.com>
2021-02-17 11:01:38 +0200

    fixed all comments

--- main.js
+++ main.js
@@ -2,34 +2,27 @@
 
 function showPeople(event) {
     event.preventDefault();
-    let n = 10;
+
+    let n = getRandomNumber(20);
     let arr = generatePeopleArray(n);
     
-    
-    arr.forEach ( function(element) {
-        let person = element; 
+    const container = document.querySelector('.list');
+    arr.forEach ( function(person, i) {
         const liElement = document.createElement('li');
-        liElement.id = "li"+ arr.indexOf(element);
+        liElement.id = "li"+ i;
         liElement.textContent = `${person.firstname} ${person.lastname} ${person.age} ${person.sex}`;
-        liElement.className = "active";
-        document.querySelector('.list').appendChild(liElement)
+        container.appendChild(liElement)
     });        
-    
-    event.target.reset();
-
-
 }
-
-const FORM = document.getElementById('form')
-FORM.addEventListener('submit', showPeople)
 
 function ready(callbackFunction){
     if(document.readyState != 'loading')
-      callbackFunction(event)
+       callbackFunction()
     else
-      document.addEventListener("DOMContentLoaded", callbackFunction)
-  }
-  ready(event => {
-    // console.log('DOM is ready.')
-    
-  })
+       document.addEventListener("DOMContentLoaded", callbackFunction)
+}
+
+ready(() => {
+    const form = document.getElementById('form');
+    form.addEventListener('submit', showPeople);
+});
